4fa97744fa6e8d6ba6d9f9ee851e3e32a9eb2b26
Brady Love <blove@pivotal.io>
2018-08-09 10:02:12 -0600

    Revert "Switch from polling to waiter diode in router egress"
    This reverts commit 911e4e6670ccee6159db4fbcb61fc4798ba558d6.

--- router/internal/server/v2/egress_server.go
+++ router/internal/server/v2/egress_server.go
@@ -1,11 +1,9 @@
 package v2
 
 import (
-	"io"
 	"log"
 	"time"
 
-	gendiode "code.cloudfoundry.org/go-diodes"
 	"code.cloudfoundry.org/go-loggregator/rpc/loggregator_v2"
 	"code.cloudfoundry.org/loggregator/diodes"
 	"code.cloudfoundry.org/loggregator/metricemitter"
@@ -31,7 +29,6 @@ type EgressServer struct {
 	egressMetric        *metricemitter.Counter
 	subscriptionsMetric *metricemitter.Gauge
 	health              HealthRegistrar
-	forceFlushInterval  time.Duration
 	batchInterval       time.Duration
 	batchSize           uint
 }
@@ -44,7 +41,6 @@ func NewEgressServer(
 	h HealthRegistrar,
 	batchInterval time.Duration,
 	batchSize uint,
-	forceFlushInterval time.Duration,
 ) *EgressServer {
 	// metric-documentation-v2: (loggregator.doppler.egress) Number of
 	// envelopes read from a diode to be sent to subscriptions.
@@ -58,7 +54,6 @@ func NewEgressServer(
 		health:              h,
 		batchInterval:       batchInterval,
 		batchSize:           batchSize,
-		forceFlushInterval:  forceFlushInterval,
 	}
 }
 
@@ -85,10 +80,7 @@ func (s *EgressServer) BatchedReceiver(
 	s.health.Inc("subscriptionCount")
 	defer s.health.Dec("subscriptionCount")
 
-	d := diodes.NewOneToOneEnvelopeV2(1000, s,
-		gendiode.WithWaiterContext(sender.Context()),
-	)
-
+	d := diodes.NewOneToOneEnvelopeV2(1000, s)
 	cancel := s.subscriber.Subscribe(req, d)
 	defer cancel()
 
@@ -103,28 +95,21 @@ func (s *EgressServer) BatchedReceiver(
 		},
 	)
 
-	dw := newDiodeWrapper(d)
-
-	t := time.NewTimer(s.forceFlushInterval)
 	for {
-		if !t.Stop() {
-			<-t.C
-		}
-		t.Reset(s.forceFlushInterval)
-
 		select {
 		case <-sender.Context().Done():
 			return sender.Context().Err()
 		case err := <-errStream:
 			return err
-		case e, ok := <-dw.next():
+		default:
+			data, ok := d.TryNext()
 			if !ok {
 				batcher.ForcedFlush()
-				return io.EOF
+				time.Sleep(10 * time.Millisecond)
+				continue
 			}
-			batcher.Write(e)
-		case <-t.C:
-			batcher.ForcedFlush()
+
+			batcher.Write(data)
 		}
 	}
 }
@@ -146,37 +131,4 @@ func (b *batchWriter) Write(batch []*loggregator_v2.Envelope) {
 		return
 	}
 	b.egressMetric.Increment(uint64(len(batch)))
-}
-
-type diodeWrapper struct {
-	d    *diodes.OneToOneEnvelopeV2
-	data chan *loggregator_v2.Envelope
-}
-
-func newDiodeWrapper(d *diodes.OneToOneEnvelopeV2) diodeWrapper {
-	dw := diodeWrapper{
-		d:    d,
-		data: make(chan *loggregator_v2.Envelope, 100),
-	}
-
-	go func() {
-		for {
-			e := dw.d.Next()
-			if e == nil {
-				close(dw.data)
-				return
-			}
-			dw.data <- e
-		}
-	}()
-
-	return dw
-}
-
-func (dw diodeWrapper) Set(e *loggregator_v2.Envelope) {
-	dw.d.Set(e)
-}
-
-func (dw diodeWrapper) next() chan *loggregator_v2.Envelope {
-	return dw.data
 }
